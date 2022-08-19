868a09f8a0cd393e9fc3755327b9bb766a12d7ff
Constantin <konstantindanilov19@hotmail.com>
2022-08-19 17:51:46 +0300

    react pizza episode 3

---
+++ react-pizza/src/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import classnames from 'classnames'
+
+const Button = (props) => {
+  return (
+    <button 
+    className={classnames('button', {
+      'button--outline': props.outline,
+    })}>
+      {props.children}
+  </button>
+  )
+}
+
+export default Button;
